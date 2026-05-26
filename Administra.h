618756af8ef987027baba86c393085d90d151a5b
Diego Scott <diegoascott@gmail.com>
2026-05-25 22:31:44 -0300

    Agrego .h Administra y Publicacion

---
+++ Administra.h
@@ -0,0 +1,31 @@
+#ifndef ADMINISTRA_H
+#define ADMINISTRA_H
+
+#include "ICollecitble.h"
+
+class Inmobiliaria;
+class Inmueble;
+class Publicacion;
+class ICollection;
+
+class Administra : public ICollectible
+{
+
+private:
+    Inmobiliaria *inmobiliaria;
+    Inmueble *inmueble;
+    ICollection *publicaciones;
+
+    Fecha : fecha;
+
+public:
+    Administra(Inmobiliaria *inmo, Inmueble *inm);
+    Inmobiliaria *getInmobiliaria();
+    Inmueble *getInmueble();
+
+    void agregarPublicacion(Publicacion *pub);
+
+    virtual ~Administra();
+}
+
+#endif
--- Sistema.h
+++ Sistema.h
@@ -11,38 +11,38 @@
 
 using namespace std;
 
-class Sistema : public ISistema {
-    private:
-        Sistema();
-        static Sistema * instance;
-        ColInmueble* colInmuebles;
-        ColUsuario* colUsuarios;
-        ColInmobiliaria* colInmobiliarias;
-        ColPublicacion* colPublicaciones;
-        //punteros a las clases que conoce sistema
+class Sistema : public ISistema
+{
+private:
+    Sistema();
+    static Sistema *instance;
+    ColInmueble *colInmuebles;
+    ColUsuario *colUsuarios;
+    ColInmobiliaria *colInmobiliarias;
+    ColPublicacion *colPublicaciones;
+    // punteros a las clases que conoce sistema
 
-    public:
-        ~Sistema();
-        static Sistema * getInstance();
-        Status revisarNickname(string) override; //override sirve para detectar errores de parametros etc etc
-        Status altaCliente(string, string, string, string, string) override;
-        Status altaPropietario(string, string, string, string, string,string) override;
-        Status altaCasa(direccion, float, int, tipoTecho, bool) override;
-        Status altaApto(direccion, float, int, int, bool, float) override;
-        Status altaInmobiliaria(string, string, string, direccion, string, string) override;
-        DTProp listarPropietarios() override;
-        void asociarPropietario(string) override;
-        DTinmobiliaria listarInmobiliarias() override;
-        DTInmuebles seleccionarInmobiliaria(string) override;
-        Status altaPublicacion(int, TipoPublicacion, string, float) override;
-        DTPublicacion listarPublicaciones(string, float, float, opciones) override;
-        DTespecifica listarEspecifica(int) override;
-        DTprop listarPropiedades() override;
-        DTinmueble mostrarDetalle(int) override;
-        Status eliminarInmueble(int) override;
-        DTInmueblesRep listarInmueblesRepresentados(string) override;
-        Status altaAdministracion(int) override;
-        
+public:
+    ~Sistema();
+    static Sistema *getInstance();
+    Status revisarNickname(string) override; // override sirve para detectar errores de parametros etc etc
+    Status altaCliente(string, string, string, string, string) override;
+    Status altaPropietario(string, string, string, string, string, string) override;
+    Status altaCasa(direccion, float, int, tipoTecho, bool) override;
+    Status altaApto(direccion, float, int, int, bool, float) override;
+    Status altaInmobiliaria(string, string, string, direccion, string, string) override;
+    DTProp listarPropietarios() override;
+    void asociarPropietario(string) override;
+    DTinmobiliaria listarInmobiliarias() override;
+    DTInmuebles seleccionarInmobiliaria(string) override;
+    Status altaPublicacion(int, TipoPublicacion, string, float) override;
+    DTPublicacion listarPublicaciones(string, float, float, opciones) override;
+    DTespecifica listarEspecifica(int) override;
+    DTprop listarPropiedades() override;
+    DTinmueble mostrarDetalle(int) override;
+    Status eliminarInmueble(int) override;
+    DTInmueblesRep listarInmueblesRepresentados(string) override;
+    Status altaAdministracion(int) override;
 };
 
 #endif
